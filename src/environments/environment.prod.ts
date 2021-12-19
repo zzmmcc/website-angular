9ee7c09604ae0b4a3d5f6cd2eb9145a0b1375a38
zhangMC <1129414986@qq.com>
2021-12-19 18:01:10 +0800

    修改脚本

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  SERVER_URL: `http://42.192.197.101:9393/website`,
+  // SERVER_URL: `http://localhost:9393/website`,
+  useHash: true,
+  hmr: false,
+  encrypt: false
 };
+
